42c295fcf98ffa56c1beb1c95f771c35de6f471d
Helton Andreazza <helton.prg@gmail.com>
2017-06-01 00:08:02 -0300

    created polyfill

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,5 @@
+import '../polyfills'; //should be loaded first for getting the es6, es7 and zone features
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
